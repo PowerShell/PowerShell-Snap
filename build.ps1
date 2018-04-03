859a10ad5238cfd248a35bdc980073bdfdd90306
Travis Plunk <travis.plunk@microsoft.com>
2018-04-03 16:49:10 -0700

    clean during simpler build

--- build.ps1
+++ build.ps1
@@ -6,7 +6,7 @@ param(
 switch($BuildType)
 {
     'Docker' {
-        docker run -v ${psscriptroot}:${psscriptroot} -w ${psscriptroot} 'snapcore/snapcraft:stable' sh -c 'apt update && snapcraft --version && snapcraft' 
+        docker run -v ${psscriptroot}:${psscriptroot} -w ${psscriptroot} 'snapcore/snapcraft:stable' sh -c 'apt update && snapcraft --version && snapcraft clean -s build && snapcraft' 
     }
     'Official' {
         &"$PSScriptRoot/tools/releaseBuild/vstsbuild.ps1" -Name powershell-snap-latest
