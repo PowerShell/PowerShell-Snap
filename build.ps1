a92ea822abfcc1a9410ffc9acdf42d246804aded
Travis Plunk <travis.plunk@microsoft.com>
2018-06-05 15:08:15 -0700

    Add third party notice generation (#4)

--- build.ps1
+++ build.ps1
@@ -1,1 +1,18 @@
-&"$PSScriptRoot/tools/releaseBuild/vstsbuild.ps1" -Name powershell-snap-latest
+param(
+    [ValidateSet('Docker','Official')]
+    [string]$BuildType="Docker"
+)
+
+switch($BuildType)
+{
+    'Docker' {
+        docker run -v ${psscriptroot}:${psscriptroot} -w ${psscriptroot} 'snapcore/snapcraft:stable' sh -c 'apt update && snapcraft --version && snapcraft clean -s build && snapcraft' 
+    }
+    'Official' {
+        &"$PSScriptRoot/tools/releaseBuild/vstsbuild.ps1" -Name powershell-snap-latest
+    }
+    default {
+        throw "Unknown BuildType: $BuildType"
+    }
+}
+
