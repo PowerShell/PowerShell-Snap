6856f2a908b94f49933ab916e913a2d82fe4a15d
Travis Plunk <travis.plunk@microsoft.com>
2018-06-30 14:23:25 -0700

    update build script to support preview builds

--- build.ps1
+++ build.ps1
@@ -1,18 +1,36 @@
 param(
     [ValidateSet('Docker','Official')]
-    [string]$BuildType="Docker"
+    [string]$BuildType="Docker",
+    [switch]$Preview
 )
+
+$directory = 'stable'
+if($Preview.IsPresent)
+{
+    $directory='preview'
+}
+
+$fullDirectory = Join-Path -Path $psscriptroot -ChildPath $directory
+
+Write-verbose -message "fd: $fullDirectory" -verbose
 
 switch($BuildType)
 {
     'Docker' {
-        docker run -v ${psscriptroot}:${psscriptroot} -w ${psscriptroot} 'snapcore/snapcraft:stable' sh -c 'apt update && snapcraft --version && snapcraft clean -s build && snapcraft' 
+        $command = "echo `$PWD && apt update && snapcraft --version && snapcraft clean -s build && snapcraft" 
+        Write-verbose -message $command -verbose
+        docker run -v ${fullDirectory}:${fullDirectory} -w ${fullDirectory} 'snapcore/snapcraft:stable' sh -c $command
     }
     'Official' {
-        &"$PSScriptRoot/tools/releaseBuild/vstsbuild.ps1" -Name powershell-snap-latest
+        $name = 'powershell-snap-latest'
+        if($Preview.IsPresent)
+        {
+            $name = 'powershell-snap-preview-latest'
+        }
+
+        &"$PSScriptRoot/tools/releaseBuild/vstsbuild.ps1" -Name $Name
     }
     default {
         throw "Unknown BuildType: $BuildType"
     }
 }
-
